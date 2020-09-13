c6536b4b76853b2bd5fbdf7134e5bdc39eade2ca
Chris <christnovie.kiala-binga@cpnv.ch>
2020-09-13 18:53:53 +0200

    Mysql_Connection

---
+++ PizzaOne/PizzaOneWithTests/ConnexionDatabase/ProgramDbConnect.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using ConnexionDatabase;
+
+
+namespace ConnexionDatabase
+{
+    class ProgramDbConnect
+    {
+        private Int16 port = 3306;
+        private string server = "localhost";
+        private string iud;
+        private string password;
+        private string database;
+        private string charset = "utf8";
+        private string sslMode = "None";
+        private string dbConnector;
+
+        static void Main(string[] args)
+        {
+            Int16 port = 3306;
+            string server = "localhost";
+            string iud = "cmysql";
+            string password="password";
+            string database = "Cdatabase";
+            string charset = "utf8";
+            string sslMode = "None";
+         
+
+        
+            string dbConnector = $"server={server};port={port};uid={iud};pwd={password};database={database};charset={charset};sslMode={sslMode}"; 
+
+            Console.WriteLine("connexion tu mysql database :");
+            try
+            {
+
+                MySqlConnection connect = new MySqlConnection(dbConnector);
+                connect.Open();
+                Console.WriteLine("Connection is " + connect.State.ToString() + Environment.NewLine);
+                MySqlCommand command = connect.CreateCommand();
+                connect.Close();
+                Console.WriteLine("Connection is " + connect.State.ToString() + Environment.NewLine);
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+
+            {
+                Console.WriteLine("Erreur : " + ex.Message.ToString());
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+
+
+
+        }
+        public  ProgramDbConnect(string user, string password,string dbname)
+        {
+            iud = user;
+            this.password = password;
+            database = dbname;
+            string con = Login;
+            
+            Console.WriteLine("connexion tu mysql database :");
+            try
+            {
+
+                MySqlConnection connect = new MySqlConnection(con);
+                connect.Open();
+                Console.WriteLine("Connection is " + connect.State.ToString() + Environment.NewLine);
+
+                connect.Close();
+                Console.WriteLine("Connection is " + connect.State.ToString() + Environment.NewLine);
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+
+            {
+                Console.WriteLine("Erreur : " + ex.Message.ToString());
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+        }
+        private string Login
+        {
+            
+            get
+            {
+                return dbConnector;
+            }
+
+            set
+            {
+               
+                dbConnector = $"server={server};port={port};uid={iud};pwd={password};database={database};charset={charset};sslMode={sslMode}";
+            }
+        }
+        
+    }
+}
---
+++ PizzaOne/PizzaOneWithTests/createdata.sql
@@ -0,0 +1,33 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.19 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for c# databaseexerce
+DROP DATABASE IF EXISTS `c# databaseexerce`;
+CREATE DATABASE IF NOT EXISTS `c# databaseexerce` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `c# databaseexerce`;
+
+-- Dumping structure for table c# databaseexerce.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID` int unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
