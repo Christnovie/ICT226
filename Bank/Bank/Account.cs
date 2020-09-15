ff83dfccde9358e7e36614ce0fa19a8cd4443d8f
christnovie <cspoutnick@gmail.com>
2020-09-15 16:55:43 +0200

    Bank

---
+++ Bank/Bank/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class Account
+    {
+        private double balance;
+        public void Deposite(double amount)
+        {
+
+        }
+        public void Withdraw(double amount)
+        {
+
+        }
+        public double Balance
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Bank/Bank/RecurringPayment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class RecurringPayment
+    {
+        private Account account;
+        private double amount;
+        private DateTime firstOccurence;
+        private TimeSpan recurringDuring;
+        public RecurringPayment(Account account,double amount)
+        {
+
+        }
+        public DateTime FirstOccurence
+        {
+            get { return firstOccurence; }
+            set { firstOccurence = value; }
+        }
+        
+        public TimeSpan RecurringDuration
+        {
+            get
+            {
+                return recurringDuring;
+            }
+            set
+            {
+                recurringDuring = value;
+            }
+        }
+       public void TriggerPayment(DateTime dueDate)
+        {
+
+        }
+
+    }
+}
